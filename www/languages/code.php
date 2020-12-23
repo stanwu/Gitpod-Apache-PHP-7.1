71923c091b85f65b7fe8c6ba5a4dace1d93699db
Stan Wu <stanwu.tw@gmail.com>
2020-12-23 12:45:20 +0000

    add codiad

---
+++ www/languages/code.php
@@ -0,0 +1,25 @@
+<?php
+$languages = array(
+    "en" => "english",
+    "fr" => "français",
+    "it" => "italiano",
+    "ru" => "русский",
+    "de" => "deutsch",
+    "es" => "español",
+    "pt" => "português",
+    "ro" => "romanian",
+    "hu" => "magyar",
+    "sv" => "swedish",
+    "cn" => "简体中文",
+    "pl" => "polish",
+    "cz" => "česky",
+    "sk" => "slovak",
+    "sr" => "српски",
+    "bg" => "Български",
+    "tr" => "Türkçe",
+    "cn" => "简体中文",
+    "tw" => "繁體中文",
+    "ja" => "日本語",
+    "nl" => "Dutch"
+);
+?>
